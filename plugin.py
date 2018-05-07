e1d1f58050c6c945f8ca83337e9435b7ca5a8e79
Xorfor <32778657+Xorfor@users.noreply.github.com>
2018-05-07 22:48:13 +0200

    Create plugin.py

---
+++ plugin.py
@@ -0,0 +1,176 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# CountDown Python Plugin
+#
+# Author: Xorfor
+#
+"""
+<plugin key="xfr_countdown" name="Countdown" author="Xorfor" version="1.0.0">
+    <params>
+        <param field="Mode1" label="Minutes" width="50px" required="true" default="10"/>
+        <param field="Mode6" label="Debug" width="75px">
+            <options>
+                <option label="True" value="Debug"/>
+                <option label="False" value="Normal" default="true"/>
+            </options>
+        </param>
+    </params>
+</plugin>
+"""
+import Domoticz
+
+
+class BasePlugin:
+
+    __HEARTBEATS2MIN = 6
+    __MINUTES = 1         # or use a parameter
+
+    # Device units
+    __UNIT_TEXT = 1
+
+    def __init__(self):
+        self.__runAgain = 0
+
+    def onStart(self):
+        Domoticz.Debug("onStart called")
+        self._minutes = int(Parameters["Mode1"])
+        self.__runAgain = self._minutes * self.__HEARTBEATS2MIN
+        if Parameters["Mode6"] == "Debug":
+            Domoticz.Debugging(1)
+        else:
+            Domoticz.Debugging(0)
+        # Create devices
+        if len(Devices) == 0:
+            Domoticz.Device(Unit=self.__UNIT_TEXT, Name="{} min.".format(Parameters["Mode1"]), TypeName="Switch", Switchtype=9, Used=1).Create()
+        UpdateDevice(self.__UNIT_TEXT, 1, "On")
+        # Log config
+        DumpConfigToLog()
+        # Connection
+
+    def onStop(self):
+        Domoticz.Debug("onStop called")
+
+    def onConnect(self, Connection, Status, Description):
+        Domoticz.Debug("onConnect called")
+
+    def onMessage(self, Connection, Data):
+        Domoticz.Debug("onMessage called")
+
+    def onCommand(self, Unit, Command, Level, Hue):
+        Domoticz.Debug(
+            "onCommand called for Unit " + str(Unit) + ": Parameter '" + str(Command) + "', Level: " + str(Level))
+        if str(Command) == "On":
+            self.__runAgain = self._minutes * self.__HEARTBEATS2MIN
+            UpdateDevice(self.__UNIT_TEXT, 1, "On", AlwaysUpdate=True)
+
+    def onNotification(self, Name, Subject, Text, Status, Priority, Sound, ImageFile):
+        Domoticz.Debug("Notification: " + Name + "," + Subject + "," + Text + "," + Status + "," + str(
+            Priority) + "," + Sound + "," + ImageFile)
+
+    def onDisconnect(self, Connection):
+        Domoticz.Debug("onDisconnect called")
+
+    def onHeartbeat(self):
+        Domoticz.Debug("onHeartbeat called")
+        if self.__runAgain > 0:
+            self.__runAgain -= 1
+            UpdateDevice(self.__UNIT_TEXT, 1, "On")
+        else:
+            UpdateDevice(self.__UNIT_TEXT, 0, "Off")
+
+global _plugin
+_plugin = BasePlugin()
+
+def onStart():
+    global _plugin
+    _plugin.onStart()
+
+def onStop():
+    global _plugin
+    _plugin.onStop()
+
+def onConnect(Connection, Status, Description):
+    global _plugin
+    _plugin.onConnect(Connection, Status, Description)
+
+def onMessage(Connection, Data):
+    global _plugin
+    _plugin.onMessage(Connection, Data)
+
+def onCommand(Unit, Command, Level, Hue):
+    global _plugin
+    _plugin.onCommand(Unit, Command, Level, Hue)
+
+def onNotification(Name, Subject, Text, Status, Priority, Sound, ImageFile):
+    global _plugin
+    _plugin.onNotification(Name, Subject, Text, Status, Priority, Sound, ImageFile)
+
+def onDisconnect(Connection):
+    global _plugin
+    _plugin.onDisconnect(Connection)
+
+def onHeartbeat():
+    global _plugin
+    _plugin.onHeartbeat()
+
+################################################################################
+# Generic helper functions
+################################################################################
+def DumpConfigToLog():
+    # Show parameters
+    Domoticz.Debug("Parameters count.....: " + str(len(Parameters)))
+    for x in Parameters:
+        if Parameters[x] != "":
+           Domoticz.Debug("Parameter '" + x + "'...: '" + str(Parameters[x]) + "'")
+    # Show settings
+        Domoticz.Debug("Settings count...: " + str(len(Settings)))
+    for x in Settings:
+       Domoticz.Debug("Setting '" + x + "'...: '" + str(Settings[x]) + "'")
+    # Show images
+    Domoticz.Debug("Image count..........: " + str(len(Images)))
+    for x in Images:
+        Domoticz.Debug("Image '" + x + "...': '" + str(Images[x]) + "'")
+    # Show devices
+    Domoticz.Debug("Device count.........: " + str(len(Devices)))
+    for x in Devices:
+        Domoticz.Debug("Device...............: " + str(x) + " - " + str(Devices[x]))
+        Domoticz.Debug("Device Idx...........: " + str(Devices[x].ID))
+        Domoticz.Debug("Device Type..........: " + str(Devices[x].Type) + " / " + str(Devices[x].SubType))
+        Domoticz.Debug("Device Name..........: '" + Devices[x].Name + "'")
+        Domoticz.Debug("Device nValue........: " + str(Devices[x].nValue))
+        Domoticz.Debug("Device sValue........: '" + Devices[x].sValue + "'")
+        Domoticz.Debug("Device Options.......: '" + str(Devices[x].Options) + "'")
+        Domoticz.Debug("Device Used..........: " + str(Devices[x].Used))
+        Domoticz.Debug("Device ID............: '" + str(Devices[x].DeviceID) + "'")
+        Domoticz.Debug("Device LastLevel.....: " + str(Devices[x].LastLevel))
+        Domoticz.Debug("Device Image.........: " + str(Devices[x].Image))
+
+def UpdateDevice(Unit, nValue, sValue, TimedOut=0, AlwaysUpdate=False):
+    if Unit in Devices:
+        if Devices[Unit].nValue != nValue or Devices[Unit].sValue != sValue or Devices[Unit].TimedOut != TimedOut or AlwaysUpdate:
+            Devices[Unit].Update(nValue=nValue, sValue=str(sValue), TimedOut=TimedOut)
+            Domoticz.Debug("Update " + Devices[Unit].Name + ": " + str(nValue) + " - '" + str(sValue) + "'")
+
+def UpdateDeviceOptions(Unit, Options={}):
+    if Unit in Devices:
+        if Devices[Unit].Options != Options:
+            Devices[Unit].Update(nValue=Devices[Unit].nValue, sValue=Devices[Unit].sValue, Options=Options)
+            Domoticz.Debug("Device Options update: " + Devices[Unit].Name + " = " + str(Options))
+
+def UpdateDeviceImage(Unit, Image):
+    if Unit in Devices and Image in Images:
+        if Devices[Unit].Image != Images[Image].ID:
+            Devices[Unit].Update(nValue=Devices[Unit].nValue, sValue=Devices[Unit].sValue, Image=Images[Image].ID)
+            Domoticz.Debug("Device Image update: " + Devices[Unit].Name + " = " + str(Images[Image].ID))
+
+def DumpHTTPResponseToLog(httpDict):
+    if isinstance(httpDict, dict):
+        Domoticz.Debug("HTTP Details (" + str(len(httpDict)) + "):")
+        for x in httpDict:
+            if isinstance(httpDict[x], dict):
+                Domoticz.Debug("....'" + x + " (" + str(len(httpDict[x])) + "):")
+                for y in httpDict[x]:
+                    Domoticz.Debug("........'" + y + "':'" + str(httpDict[x][y]) + "'")
+            else:
+                Domoticz.Debug("....'" + x + "':'" + str(httpDict[x]) + "'")
